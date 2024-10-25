dc7f890e4d3cf012cdd568e66f23f1975a8ab87d
Vinkal Vishnoi <vinkal@athmin.com>
2024-10-25 13:12:19 +0530

    webhook details

--- src/dbhelper/webhookdetailsdao.js
+++ src/dbhelper/webhookdetailsdao.js
@@ -1,5 +1,5 @@
 const { createMongoAndFilterForColumns, modifyMongoQuery } = require("./mongohelpers");
-const WebhookDetailsDo = require('../models/webhookDetails');
+const WebhookDetailsDo = require('../models/webhookdetails');
 
 const FILTER_COLUMNS = ['automation_id', 'pronnel_webhook_url', 'connection_id', '_id', 'trigger_type'];
 const RANGE_COLUMNS = ['create_date'];
