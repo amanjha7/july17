9aa7c8126d0e7ba0c820f4b06e09278886d1df14
Mohit Bansal <mohit1211@gmail.com>
2024-11-04 13:59:16 +0530

    Fixed approutes

--- src/routes/approutes.js
+++ src/routes/approutes.js
@@ -2,21 +2,11 @@ const express = require('express');
 const {
   handleSubscription,
   handleUnsubscription,
-  createConnection,
   sendWebhookSample,
-  updateConnection,
-  receiveWebhook,
-  handleCallback,
-  handleAuthInitiation,
-  checkConnectionValidity,
-  revokeAccessToken,
+  receiveWebhook
 } = require('../controllers/appcontroller');
 const { authenticationMiddleware } = require('../middlewares/authentication');
 const router = express.Router();
-
-//GET endpoint to receive the auth call from oauth initiator application
-router.get('/auth', authenticationMiddleware, handleAuthInitiation);
-router.get('/callback', handleCallback)
 
 // POST endpoint for subscription of the pronnel webhook
 router.post('/subscribe', authenticationMiddleware, handleSubscription);
@@ -26,12 +16,6 @@ router.post('/unsubscribe', authenticationMiddleware, handleUnsubscription);
 
 // Webhook - for sending sample response to pronnel
 router.get("/webhook/sample", sendWebhookSample);
-
-// POST endpoints related to connection
-router.post('/connection', authenticationMiddleware, createConnection);
-router.patch('/connection/update', authenticationMiddleware, updateConnection);
-router.post("/connection/validate", checkConnectionValidity);
-router.delete("/auth/revoke", revokeAccessToken);
 
 // Webhook - for configuration in the app
 router.post("/common/webhook", receiveWebhook);
