31c83029bc5b85eaa42bc882e85331a38e6045fc
Vinkal <vinkal@athmin.com>
2024-11-07 07:51:31 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 // Load environment variables from .env file
-require('dotenv').config();
+require('dotenv').config({
+    path: (process.env.NODE_ENV)+'.env'
+}
+);
 const cors = require('cors');
 const express = require('express');
 const session = require('express-session');
